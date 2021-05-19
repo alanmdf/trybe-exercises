4c825b400a57e07165ec6334efd758767dede05d
alanmdf <alanmoreirascr@gmail.com>
2021-05-19 19:58:15 -0300

    Exercicios 10 e 11

--- exercises/MODULO_1/bloco_4/dia_1/exercicio_pos_aula.js
+++ exercises/MODULO_1/bloco_4/dia_1/exercicio_pos_aula.js
@@ -173,4 +173,158 @@ let mod = a % b;
 // Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
 // O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
 // valorCustoTotal = valorCusto + impostoSobreOCusto
-// lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+// let custoProduto = 10;
+// let valorVenda = 20;
+// let taxaImposto = 1.2;
+// let custoTotal = custoProduto * taxaImposto;
+
+// if (custoProduto < 0) {
+//   console.log("Erro. Valor do custo inválido.");
+// } else if (valorVenda <0) {
+//   console.log("Erro. Valor da venda inválida.");
+// } else {
+//   let lucroTotal = (valorVenda - custoTotal) * 1000;
+//   console.log("Seu lucro é de: R$ " + lucroTotal)
+// }
+
+// 11. Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+// INSS (Instituto Nacional do Seguro Social)
+// Salário bruto até R$ 1.556,94: alíquota de 8%
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+// IR (Imposto de Renda)
+// Até R$ 1.903,98: isento de imposto de renda
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+// Exemplo : Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
+// O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
+// Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
+// Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, temos:
+// R$ 2.670,00: salário com INSS já deduzido;
+// 7.5%: alíquota de imposto de renda;
+// R$ 142,80 parcela a se deduzir do imposto.
+// Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
+// O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+// Resultado: R$ 2.612,55.
+
+// let salarioBruto = 10000;
+// let inssOitoPorcento = 0.08;
+// let inssNovePorcento = 0.09;
+// let inssOnzePorcento = 0.11;
+// let inssMax= 570.88;
+// let irSeteEMeioPorcento = 0.075;
+// let irQuinzePorcento = 0.15;
+// let irVinteEDoisEMeioPorcento = 0.225;
+// let irVinteESeteEMeioPorcento = 0.275;
+
+// if (salarioBruto <= 1556.94) {
+//   let salarioDescontoInss = salarioBruto - (salarioBruto * inssOitoPorcento);
+//   salarioDescontoInss = salarioDescontoInss.toFixed(2);
+//   if (salarioDescontoInss <= 1903.98) {
+//     console.log('Seu salário é: R$ ' + salarioDescontoInss);
+//   } else if (salarioDescontoInss <= 2.826,65) {
+//     taxaDescontoIr = (salarioDescontoInss * irSeteEMeioPorcento) - 142,80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 3751.05) {
+//     taxaDescontoIr = (salarioDescontoInss * irQuinzePorcento) - 354.80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 4664.68) {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteEDoisEMeioPorcento) - 636.13;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteESeteEMeioPorcento) - 869.36;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   }
+// } else if (salarioBruto <= 2594.92) {
+//   let salarioDescontoInss = salarioBruto - (salarioBruto * inssNovePorcento);
+//   salarioDescontoInss = salarioDescontoInss.toFixed(2);
+//   if (salarioDescontoInss <= 1903.98) {
+//     console.log('Seu salário é: R$ ' + salarioDescontoInss);
+//   } else if (salarioDescontoInss <= 2.826,65) {
+//     taxaDescontoIr = (salarioDescontoInss * irSeteEMeioPorcento) - 142,80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 3751.05) {
+//     taxaDescontoIr = (salarioDescontoInss * irQuinzePorcento) - 354.80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 4664.68) {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteEDoisEMeioPorcento) - 636.13;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteESeteEMeioPorcento) - 869.36;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   }
+// } else if (salarioBruto <= 5189.82) {
+//   let salarioDescontoInss = salarioBruto - (salarioBruto * inssOnzePorcento);
+//   salarioDescontoInss = salarioDescontoInss.toFixed(2);
+//   if (salarioDescontoInss <= 1903.98) {
+//     console.log('Seu salário é: R$ ' + salarioDescontoInss);
+//   } else if (salarioDescontoInss <= 2.826,65) {
+//     taxaDescontoIr = (salarioDescontoInss * irSeteEMeioPorcento) - 142,80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 3751.05) {
+//     taxaDescontoIr = (salarioDescontoInss * irQuinzePorcento) - 354.80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 4664.68) {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteEDoisEMeioPorcento) - 636.13;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteESeteEMeioPorcento) - 869.36;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   }
+// } else {
+//   let salarioDescontoInss = salarioBruto - inssMax;
+//   salarioDescontoInss = salarioDescontoInss.toFixed(2);
+//   if (salarioDescontoInss <= 1903.98) {
+//     console.log('Seu salário é: R$ ' + salarioDescontoInss);
+//   } else if (salarioDescontoInss <= 2.826,65) {
+//     taxaDescontoIr = (salarioDescontoInss * irSeteEMeioPorcento) - 142,80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 3751.05) {
+//     taxaDescontoIr = (salarioDescontoInss * irQuinzePorcento) - 354.80;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else if (salarioDescontoInss <= 4664.68) {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteEDoisEMeioPorcento) - 636.13;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   } else {
+//     taxaDescontoIr = (salarioDescontoInss * irVinteESeteEMeioPorcento) - 869.36;
+//     salarioDescontoIr = salarioDescontoInss - taxaDescontoIr;
+//     salarioDescontoIr = salarioDescontoIr.toFixed(2);
+//     console.log('Seu salário é: R$ ' + salarioDescontoIr);
+//   }
+// }
+
