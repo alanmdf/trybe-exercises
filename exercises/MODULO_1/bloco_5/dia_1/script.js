8403345f871d508893a59c0d237a010ea37596b2
alanmdf <alanmoreirascr@gmail.com>
2021-05-26 18:11:13 -0300

    Exercicios pre-aula, falta finalizar

---
+++ exercises/MODULO_1/bloco_5/dia_1/script.js
@@ -0,0 +1,7 @@
+let customizeHeaderContainer = document.querySelector('#header-container');
+customizeHeaderContainer.style.backgroundColor = 'rgb(0, 176, 105)';
+let customizeEmergencyTasks = document.querySelectorAll('.emergency-tasks h3');
+customizeEmergencyTasks[0].style.backgroundColor = 'rgb(165, 0, 243)';
+customizeEmergencyTasks[1].style.backgroundColor = 'rgb(165, 0, 243)';
+let customizeEmergencyTasksSection = document.querySelector('.emergency-tasks div');
+customizeEmergencyTasks[0].style.backgroundColor = 'rgb(255, 159, 132)';
