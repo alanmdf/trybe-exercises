c464115571a797118141444f049013a6cf61c909
alanmdf <alanmoreirascr@gmail.com>
2021-06-18 15:21:28 -0300

    Exercicio pre aula Parte I

---
+++ exercises/MODULO_1/bloco_7/dia_2/exercicio_pre_aula_parte1.js
@@ -0,0 +1,8 @@
+const creatingObject = (objectName, objectKey, objectKeyValue) => {
+  objectName = { };
+  objectName[objectKey] = objectKeyValue;
+  console.log(objectName);
+}
+
+creatingObject('teste', 'testeChave', 'teste-valor');
+
