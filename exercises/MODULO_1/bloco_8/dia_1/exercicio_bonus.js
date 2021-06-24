44e7ab731a0c08e0a29c56150b30df69a2247afb
alanmdf <alanmoreirascr@gmail.com>
2021-06-24 11:36:50 -0300

    Exercicio Bonus finalizado e melhorado

--- exercises/MODULO_1/bloco_8/dia_1/exercicio_bonus.js
+++ exercises/MODULO_1/bloco_8/dia_1/exercicio_bonus.js
@@ -1,9 +1,7 @@
-const assert = require('assert');
-
 const mage = {
   healthPoints: 130,
   intelligence: 45,
-  mana: 125,
+  mana: 75,
   damage: undefined,
 };
 
@@ -15,7 +13,7 @@ const warrior = {
 };
 
 const dragon = {
-  healthPoints: 350,
+  healthPoints: 700,
   strength: 50,
   damage: undefined,
 };
@@ -24,32 +22,87 @@ const battleMembers = { mage, warrior, dragon };
 
 const dragonDamage = () => {
   let damage = Math.floor((Math.random() * (dragon.strength - 15 + 1)) + 15);
-  return damage
-}
+  return damage;
+};
 
 const warriorDamage = () => {
   let damage = Math.floor((Math.random() * ((warrior.strength * warrior.weaponDmg) - warrior.strength + 1) + warrior.strength));
-  return damage
-}
+  return damage;
+};
 
 const mageDamage = () => {
   let damage = Math.floor((Math.random() * ((mage.intelligence * 2) - mage.intelligence + 1) + mage.intelligence));
-  return damage
-}
+  return damage;
+};
 
 const mageDmgAndMana = () => {
-  if (mage.mana => 15) {
-    let object = {
+  let object = {};
+  if (mage.mana >= 15) {
+    object = {
     damage: mageDamage(),
-    manaSpent: 15    
-  } 
+    manaSpent: 15
+    }
   } else {
-    let object = {
-      damage: 'Não possui mana suficiente'
+    object = {
+      damage: 0,
       manaSpent: 0
     }
   }
-  return object
+  return object;
+};
+
+const gameActions = {
+  warriorTurn: (warriorDamage) => {
+    let damage = warriorDamage();
+    dragon.healthPoints -= damage;
+    warrior.damage = damage;
+    return battleMembers
+  },
+  mageTurn: (mageDmgAndMana) => {
+    let object = mageDmgAndMana();
+    dragon.healthPoints -= object.damage;
+    mage.damage = object.damage;
+    mage.mana = mage.mana > 0 ? mage.mana - object.manaSpent : 'Não possui mana suficiente';
+    return battleMembers;
+  },
+  dragonTurn: (dragonDamage) => {
+    let damage = dragonDamage();
+    warrior.healthPoints -= damage;
+    mage.healthPoints -= damage;
+    dragon.damage = damage;
+    return battleMembers;
+  },
+  showTurnResult: () => {
+    return console.log(battleMembers);
+  }
+};
+
+for (let index = 0; index < 10; index += 1) {
+  if (dragon.healthPoints <= 0) {
+    console.log('Parabéns, você venceu');
+    break;
+  }
+  if (warrior.healthPoints > 0) {
+    gameActions.warriorTurn(warriorDamage);
+  } else {
+    console.log('O guerreiro está morto');
+  }
+  if (mage.healthPoints > 0) {
+    gameActions.mageTurn(mageDmgAndMana);
+  } else {
+    console.log('O mago está morto');
+  }
+  if (warrior.healthPoints <= 0 && mage.healthPoints <= 0) {
+    console.log('Que pena, você perdeu');
+    break;
+  }
+  if (dragon.healthPoints > 0) {
+    gameActions.dragonTurn(dragonDamage);
+  }  
+  gameActions.showTurnResult()
 }
+
+
+
 
 
