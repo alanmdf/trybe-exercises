8f0080da6181cb4373f140907e7a922e59d69e5c
alanmdf <alanmoreirascr@gmail.com>
2021-07-15 18:43:39 -0300

    Exercicio 2 finalizado

--- exercises/MODULO_1/bloco_10/dia_3/tests/exercise1.test.js
+++ exercises/MODULO_1/bloco_10/dia_3/tests/exercise1.test.js
@@ -9,3 +9,13 @@ describe('When using genRandomNumber', () => {
     expect(genRandomNumberMocked).toHaveBeenCalledTimes(1);
   });
 });
+
+const genRandomNumberMocked2 = jest.fn(genRandomNumber).mockImplementationOnce((a, b) => a / b);
+
+describe('New implementation in the function', () => {
+  it('should return a division', () => {
+    expect(genRandomNumberMocked2(4, 2)).toBe(2);
+    expect(genRandomNumberMocked2).toHaveBeenCalled();
+    expect(genRandomNumberMocked2).toHaveBeenCalledTimes(1);expect(genRandomNumberMocked2).toHaveBeenCalledWith(4, 2);
+  });
+});
